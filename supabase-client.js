342c540cdcd584a6d1258fcb187b447c323578cd
Thibaut C. <20838877+Onirim@users.noreply.github.com>
2026-06-09 16:00:51 +0200

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
 // ── Config Supabase partagée ──────────────────────────────────
-const SUPABASE_URL = 'https://hknelrnwdcoqdgodtuwc.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_0NulxPP52dIAUMXcYoHrcA_z7NAfeW5';
+const SUPABASE_URL = 'https://bpkjjyoyzugibfvupjso.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_X5gDfq3Jl2EnrdX7o8kbgw_mkEHTnW3';
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
